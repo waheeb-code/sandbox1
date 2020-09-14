6afd8fd3f341121a8ac5d234214fe38a2a730b75
waheeb alshamiry <whaib77@gmail.com>
2020-09-14 05:50:08 +0300

    First commit

---
+++ src/typeintro.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var experienceType =5
+    val PlayerName = "waheeb"
+
+
+
+    experienceType=8
+
+println()
+    println(PlayerName)
+    println(experienceType)
+
+
+
+    var hasSteed = 0
+    println(PlayerName.reversed())
+}
+
+class TypeIntro {
+}
